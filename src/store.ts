e975617bc5f620b72698a6b484007eae70d9a680
agustinjob <agus_job@hotmail.com>
2024-08-20 18:52:37 -0600

    finish

---
+++ src/store.ts
@@ -0,0 +1,44 @@
+import { create } from 'zustand';
+import { Cryptocurrency, CryptoPrice, Pair } from './types';
+import { devtools } from 'zustand/middleware';
+import { fetchCurrentCryptoPrice, getCryptos } from './services/CryptoService';
+
+type CryptoStore = {
+    cryptocurrencies: Cryptocurrency[]
+    result: CryptoPrice
+    loading: boolean
+    fetchCryptos: () => Promise<void>
+    fetchData: (pair: Pair) => Promise<void>
+}
+
+export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
+    cryptocurrencies: [],
+    result: {
+        IMAGEURL: '',
+        PRICE: '',
+        HIGHDAY: '',
+        LOWDAY: '',
+        CHANGEPCT24HOUR: '',
+        LASTUPDATE: ''
+    },
+    loading: false,
+    fetchCryptos: async () => {
+        const cryptocurrencies = await getCryptos()
+        set(() => ({
+            cryptocurrencies
+        }))
+    },
+    fetchData: async (pair) => {
+        set(() => ({
+            loading: true
+        }))
+        const result = await fetchCurrentCryptoPrice(pair)
+        console.log(result)
+        set(() => ({
+            result
+        }))
+        set(() => ({
+            loading: false
+        }))
+    }
+})))
